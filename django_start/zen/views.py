ab4cb7fe243276e4a947925ad9754d21b75e80d8
Konstantin <br3d89@ukr.net>
2016-11-10 11:13:46 +0200

    Models changed

--- django_start/zen/views.py
+++ django_start/zen/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+import random
+import this
 # Create your views here.
+from django.test import TestCase
+
+# Create your tests here.
+
+text = ''.join([this.d.get(c,c) for c in this.s])
+title, _, *quotes = text.splitlines()
+
+def index(request):
+    return render(request, 'index.html', {'title':title,'quote':random.choice(quotes)})

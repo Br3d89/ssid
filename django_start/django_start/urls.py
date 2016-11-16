7c12c30744fceab2c07e575e009d783c8f2f4916
Konstantin <br3d89@ukr.net>
2016-11-16 17:57:42 +0200

    Models changed

--- django_start/django_start/urls.py
+++ django_start/django_start/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from zen.views import index
+#from zen.views import index
+from courses.views import index
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^$', index),
+    url(r'^admin/', admin.site.urls),
 ]

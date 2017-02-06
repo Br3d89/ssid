c25cfbb1d426c93760cc858e9346792374b93fb4
Konstantin <br3d89@ukr.net>
2017-02-06 17:19:47 +0200

    added jquery features

--- ssidswitcher1/ssid/views.py
+++ ssidswitcher1/ssid/views.py
@@ -71,9 +71,9 @@ def cisco(up_new, down_new, ssid_objects, i, ssid_status):
     #child = pexpect.spawn('ssh -l {} {}'.format(ssh_username, i))
     child = pexpect.spawn('telnet {}'.format(i))
     child.expect(':', timeout=30)
-    child.sendline(ssh_username)
+    child.sendline(ssh_username+"\n")
     child.expect(':', timeout=30)
-    child.sendline(ssh_password)
+    child.sendline(ssh_password+"\n")
     for m in ssid_objects:
         child.expect(">" ,timeout=30)
         if m.name in up_new:

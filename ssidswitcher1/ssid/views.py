91be04cb37c63214c0d777926fdc8173b6c2ea0e
Konstantin <br3d89@ukr.net>
2017-02-13 17:44:33 +0200

    added jquery features

--- ssidswitcher1/ssid/views.py
+++ ssidswitcher1/ssid/views.py
@@ -76,33 +76,19 @@ def ssid_update(request):
 def cisco(up_new, down_new, ssid_objects, i, ssid_status, errors, t=0):
     print('Executing SSH command cisco t=', t)
     try:
-        child = pexpect.spawn('ssh -l {} -oStrictHostKeyChecking=no {}'.format(ssh_username, i))
-        # except pexpect.exceptions.TIMEOUT as err:
-        #    errors.append(err)
-        #    print(err)
-        # child = pexpect.spawn('telnet {}'.format(i))
-        print('Waiting for Username:', 'Before:', child.before, 'After:', child.after)
+        child = pexpect.spawn('ssh -l {} -o StrictHostKeyChecking=no {}'.format(ssh_username, i))
         child.expect('User:')
         child.sendline(ssh_username)
-        print('Waiting for Password:', 'Before:', child.before, 'After:', child.after)
         child.expect('Password:')
         child.sendline(ssh_password)
-        print('Starting for loop and waiting for >', 'Before:', child.before, 'After:', child.after)
         for m in ssid_objects:
             child.expect(">")
-            print('Received expected >', 'Before:', child.before, 'After:', child.after)
-            # if t==0:
-            if ((m.name in up_new) and t == 0):
+            if (m.name in up_new) and t == 0:
                 child.sendline('config wlan enable {}'.format(m.wlan_id))
                 m.status = 1
             else:
                 child.sendline('config wlan disable {}'.format(m.wlan_id))
                 m.status = 0
-            # else:
-            #    child.sendline('config wlan disable {}'.format(m.wlan_id))
-            #    m.status = 0
-            #    global down_status
-            #    down_status.append(m.name)
             m.save()
             ssid_status.append(m.name)
         child.expect('>')
@@ -118,7 +104,7 @@ def cisco(up_new, down_new, ssid_objects, i, ssid_status, errors, t=0):
 
 def aruba(up_new, down_new, ssid_objects, i, ssid_status, errors, t=0):
     print('Eceuting ssh command aruba')
-    child = pexpect.spawn('ssh -l {} {}'.format(ssh_username, i))
+    child = pexpect.spawn('ssh -l {} {} -o StrictHostKeyChecking=no'.format(ssh_username, i))
     child.expect(":")
     child.sendline("{}\r".format(ssh_password))
     child.expect("#")
@@ -127,7 +113,7 @@ def aruba(up_new, down_new, ssid_objects, i, ssid_status, errors, t=0):
     for m in ssid_objects:
         child.sendline('wlan ssid-profile {}\r'.format(m.wlan_id))
         child.expect('#')
-        if ((m.name in up_new) and t == 0):
+        if (m.name in up_new) and t == 0:
             child.sendline('enable\r')
             child.expect('#')
             m.status = 1
@@ -142,6 +128,106 @@ def aruba(up_new, down_new, ssid_objects, i, ssid_status, errors, t=0):
     child.sendline('commit apply\r')
     child.expect('#')
     child.sendline('logout')
+
+
+def unifi(up_new, down_new, ssid_objects, i, ssid_status, errors, t=0):
+    child = pexpect.spawn('ssh -l {} -o StrictHostKeyChecking=no {}'.format(ssh_username,i))
+    child.expect('password')
+    child.sendline(ssh_password)
+    for m in ssid_objects:
+        child.expect('#')
+        if ((m.name in up_new) and t == 0):
+            child.sendline('ifconfig wifi0 up')
+            child.expect('#')
+            child.sendline('reboot')
+            m.status = 1
+        else:
+            child.sendline('ifconfig wifi0 down')
+            child.expect('#')
+            child.sendline('exit')
+            m.status = 0
+        m.save()
+        ssid_status.append(m.name)
+
+def mikrotik(up_new, down_new, ssid_objects, i, ssid_status, errors, t=0):
+    child = pexpect.spawn('ssh -l {} -o StrictHostKeyChecking=no {}'.format(ssh_username,i))
+    child.expect('password')
+    child.sendline('{}\n\r'.format(ssh_password))
+    for m in ssid_objects:
+        child.expect('>')
+        if (m.name in up_new) and t == 0:
+            child.sendline("/interface wireless enable {}\n\r".format(m.wlan_id))
+            m.status = 1
+        else:
+            child.sendline("/interface wireless disable {}\n\r".format(m.wlan_id))
+            m.status = 0
+        m.save()
+        ssid_status.append(m.name)
+    child.expect('>')
+    child.sendline('/quit\n\r')
+
+def ruckus(up_new, down_new, ssid_objects, i, ssid_status, errors, t=0):
+    child = pexpect.spawn('ssh -l {} -o StrictHostKeyChecking=no {}'.format(ssh_username, i))
+    child.expect('login:')
+    child.sendline(ssh_username)
+    child.expect('Password:')
+    child.sendline(ssh_password)
+    child.expect('>')
+    child.sendline('enable')
+    child.expect('#')
+    child.sendline('config')
+    for m in ssid_objects:
+        child.expect('#')
+        if (m.name in up_new) and t == 0:
+            child.sendline('wlan {}'.format(m.wlan_id))
+            child.expect('#')
+            child.sendline('type hotspot {}'.format(m.wlan_id))
+            m.status = 1
+        else:
+            child.sendline('no wlan {}'.format(m.wlan_id))
+            m.status = 0
+        child.expect('#')
+        child.sendline('end')
+        child.expect('#')
+        m.save()
+        ssid_status.append(m.name)
+    child.sendline('end')
+    child.expect('#')
+    child.sendline('exit')
+
+def openwrt(up_new, down_new, ssid_objects, i, ssid_status, errors, t=0):
+    child = pexpect.spawn('ssh -l {} -o StrictHostKeyChecking=no {}'.format('root', i))
+    child.expect('password:')
+    child.sendline('AQ!SW@de3?')
+    for m in ssid_objects:
+        child.expect('#')
+        if (m.name in up_new) and t == 0:
+            child.sendline('uci set wireless.@wifi-device[0].disabled=0; uci commit wireless; wifi\n')
+            m.status = 1
+        else:
+            child.sendline('uci set wireless.@wifi-device[0].disabled=1; uci commit wireless; wifi\n')
+            m.status = 0
+        child.expect('#')
+        m.save()
+        ssid_status.append(m.name)
+    child.expect('#')
+    child.sendline('exit')
+
+
+def meraki(up_new, down_new, ssid_objects, i, ssid_status, errors, t=0):
+    murl = 'https://n150.meraki.com/api/v0/organizations/616518/networks/N_647392446434529213/ssids/'
+    headers = {'X-Cisco-Meraki-API-Key': 'b21b5c3bfa37f5d920831f11775a321c077e71d1','Content-Type': 'application/json'}
+    for m in ssid_objects:
+        url = murl + m.wlan_id
+        if (m.name in up_new) and t == 0:
+            putdata = {'enabled': True}
+            m.status = 1
+        else:
+            putdata = {'enabled': False}
+            m.status = 0
+        dashboard = requests.put(url, data=json.dumps(putdata), headers=headers)
+        m.save()
+        ssid_status.append(m.name)
 
 
 
@@ -161,317 +247,8 @@ def index(request):
     ctx['ok']='Run'
     if request.method == 'POST':
         return HttpResponse('Index not for POSTs')
-        #all_up_ssids = list(ssid.objects.values_list('name', flat=True).filter(status='1'))
-        #return JsonResponse({'all_up_ssids': all_up_ssids, 'errors': errors})
     else:
         return render(request, 'index.html', ctx)
-     #up_new=json.loads(request.POST.get("up"))
-        '''down_new=json.loads(request.POST.get('down'))
-        timeout_value= json.loads(request.POST.get('timer'))
-        print(timeout_value)
-        rcv_ssids=up_new+down_new
-        ip_list=set(ssid.objects.values_list('ip', flat=True).filter(name__in=rcv_ssids))
-        process_list=[]
-        for i in ip_list:
-            vendor = list(set(ssid.objects.values_list('vendor', flat=True).filter(ip=i)))[0]
-            ssid_objects=ssid.objects.filter(ip=i, name__in=rcv_ssids) #all ssids within device
-            p=(threading.Thread(target=globals()['{}'.format(vendor)],args=(up_new, down_new, ssid_objects, i, ssid_status,errors)))
-            #p = Process(target=globals()['{}'.format(vendor)], args=(up_new, down_new, ssid_objects, i, ssid_status))
-            p.start()
-            if (len(down_new)==0):
-                threading.Timer(timeout_value, globals()['{}'.format(vendor)],args=(up_new, down_new, ssid_objects, i, ssid_status, errors,1)).start()
-            process_list.append(p)
-        for i in process_list:
-            print('Starting ', i)
-            #i.start()
-            i.join()
-            #if vendor == 'cisco':
-            #    cisco(up_new,down_new,ssid_objects,i,ssid_status)
-            #elif vendor == 'aruba':
-            #    aruba(up_new, down_new, ssid_objects, i, ssid_status)
-            #elif vendor == 'ruckus':pass
-         all_up_ssids = list(ssid.objects.values_list('name', flat=True).filter(status='1'))
-         return JsonResponse({'all_up_ssids':all_up_ssids,'errors':errors})
-    else:
-        return render(request, 'index.html', ctx)
-
-
-def cisco(up_new, down_new, ssid_objects, i, ssid_status,errors,t=0):
-    print('Executing SSH command cisco t=',t)
-    try:
-        child = pexpect.spawn('ssh -l {} -oStrictHostKeyChecking=no {}'.format(ssh_username, i))
-    #except pexpect.exceptions.TIMEOUT as err:
-    #    errors.append(err)
-    #    print(err)
-    #child = pexpect.spawn('telnet {}'.format(i))
-        print('Waiting for Username:','Before:', child.before,'After:', child.after)
-        child.expect('User:')
-        child.sendline(ssh_username)
-        print('Waiting for Password:', 'Before:', child.before, 'After:', child.after)
-        child.expect('Password:')
-        child.sendline(ssh_password)
-        print('Starting for loop and waiting for >', 'Before:', child.before, 'After:', child.after)
-        for m in ssid_objects:
-            child.expect(">")
-            print('Received expected >', 'Before:', child.before, 'After:', child.after)
-            #if t==0:
-            if ((m.name in up_new) and t==0):
-                child.sendline('config wlan enable {}'.format(m.wlan_id))
-                m.status = 1
-            else:
-                child.sendline('config wlan disable {}'.format(m.wlan_id))
-                m.status = 0
-            #else:
-            #    child.sendline('config wlan disable {}'.format(m.wlan_id))
-            #    m.status = 0
-            #    global down_status
-            #    down_status.append(m.name)
-            m.save()
-            ssid_status.append(m.name)
-        child.expect('>')
-        child.sendline('logout')
-        child.expect('(y/N)')
-        child.sendline('y')
-        print('Backend success')
-    except pexpect.exceptions.TIMEOUT as err:
-        errors.append(err)
-        #print('Error Br3d',err)
-        print('Br3d pexpect time error')
-
-
-def aruba(up_new, down_new, ssid_objects, i, ssid_status,errors,t=0):
-    print('Eceuting ssh command aruba')
-    child = pexpect.spawn('ssh -l {} {}'.format(ssh_username,i))
-    child.expect(":")
-    child.sendline("{}\r".format(ssh_password))
-    child.expect("#")
-    child.sendline('conf\r')
-    child.expect('#')
-    for m in ssid_objects:
-        child.sendline('wlan ssid-profile {}\r'.format(m.wlan_id))
-        child.expect('#')
-        if ((m.name in up_new) and t == 0):
-            child.sendline('enable\r')
-            child.expect('#')
-            m.status = 1
-        else:
-            child.sendline('disable\r')
-            child.expect('#')
-            m.status = 0
-        m.save()
-        ssid_status.append(m.name)
-    child.sendline('end\r')
-    child.expect('#')
-    child.sendline('commit apply\r')
-    child.expect('#')
-    child.sendline('logout')
-    time.sleep(10)
-
-def cisco(up_new,down_new,ssid_objects,i,ssid_status):
-    #child = pexpect.spawn('ssh -l {} {}'.format(ssh_username, i))
-    child = pexpect.spawn('telnet {}'.format(i))
-    child.expect(':')
-    child.sendline(ssh_username)
-    child.expect(':')
-    child.sendline(ssh_password)
-    for m in ssid_objects:
-        child.expect(">")
-        if m.name in up_new:
-            child.sendline('config wlan enable {}'.format(m.wlan_id))
-            m.status = 1
-        else:
-            child.sendline('config wlan disable {}'.format(m.wlan_id))
-            m.status = 0
-        m.save()
-        ssid_status.append(m.name)
-    child.expect('>')
-    child.sendline('logout')
-    child.expect('(y/N)')
-    child.sendline('y')
-
-
-def aruba(up_new,down_new,ssid_objects,i,ssid_status):
-    child = pexpect.spawn('telnet {}'.format(i))
-    child.expect(":")
-    child.sendline("{} + \r".format(ssh_username))
-    child.expect(":")
-    child.sendline("{} + \r".format(ssh_password))
-    child.expect("#")
-    child.sendline('conf' + '\r')
-    child.expect('#')
-    for m in ssid_objects:
-        child.sendline('wlan ssid-profile {} \r'.format(m.wlan_id))
-        child.expect('#')
-        if m.name in up_new:
-            child.sendline('enable \r')
-            child.expect('#')
-            m.status = 1
-        else:
-            child.sendline('disable \r')
-            child.expect('#')
-            m.status = 0
-        m.save()
-        ssid_status.append(m.name)
-    child.sendline('end' + '\r')
-    child.expect('#')
-    child.sendline('commit apply' + '\r')
-    child.expect('#')
-    child.sendline('logout')
-
-
-
-def change(ssid,device_ip,vendor,state):
-    #username = 'mgmt'
-    #password = 'Ve7petrU'
-    #remote_conn_pre = paramiko.SSHClient()
-    #remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    #remote_conn_pre.connect(device_ip, username=username, password=password, look_for_keys=False, allow_agent=False)
-    if vendor == 'cisco':
-        #remote_conn = remote_conn_pre.invoke_shell()
-        remote_conn=sshp_shell(device_ip)
-        print(ssid,remote_conn)
-        remote_conn.send("mgmt\n")
-        time.sleep(1)
-        remote_conn.send("Ve7petrU\n")
-        time.sleep(2)
-        remote_conn.send("show wlan summary\n")
-        time.sleep(1)
-        output = remote_conn.recv(5000)
-        wlanlist = output.decode("utf-8").split('\n')[5:]
-        for i in wlanlist:
-            if ssid in i:
-                wlan_id = i.split()[0]
-                break
-        if state:
-            remote_conn.send('config wlan enable {}\n'.format(int(wlan_id)))
-        else:
-            remote_conn.send('config wlan disable {}\n'.format(int(wlan_id)))
-        time.sleep(2)
-        remote_conn.close()
-    elif vendor=='aruba':
-        remote_conn = sshp_shell(device_ip)
-        print(ssid,remote_conn)
-        time.sleep(1)
-        remote_conn.send("conf t\n")
-        time.sleep(1)
-        remote_conn.send("wlan ssid-profile {}\n".format(ssid))
-        time.sleep(1)
-        print('Wlan-config Aruba {}'.format(ssid))
-        if state:
-            remote_conn.send("enable\n")
-            print('Wlan-enable Aruba {}'.format(ssid))
-            time.sleep(1)
-        else:
-            remote_conn.send("disable\n")
-            print('Wlan-disable Aruba {}'.format(ssid))
-            time.sleep(1)
-        remote_conn.send("end\n")
-        time.sleep(1)
-        print('End config Aruba {}'.format(ssid))
-        remote_conn.send("commit apply\n")
-        time.sleep(1)
-        print('Commit config Aruba {}'.format(ssid))
-        remote_conn.send("logout\n")
-        time.sleep(1)
-        print('Logout Aruba {}, {}'.format(ssid,remote_conn))
-        remote_conn.close()
-    elif vendor=="mikrotik":
-        remote_conn_pre = sshp_rcmd(device_ip)
-        if state == 0:
-            remote_conn_pre.exec_command("/interface wireless disable {}".format(ssid))
-            time.sleep(1)
-        else:
-            remote_conn_pre.exec_command("/interface wireless enable {}".format(ssid))
-            time.sleep(1)
-        remote_conn_pre.close()
-    elif vendor=="ruckus":
-        #child = pexpect.spawn('telnet ' +device_ip)
-        #child.expect('Please login: ')
-        #child.sendline(username)
-        #child.expect("Password: ")
-        #child.sendline(password)
-        #child.expect("ruckus> ")
-        #child.sendline('enable force')
-        #child.expect("ruckus# ")
-        #child.sendline('conf')
-        #child.expect(".*config.*")
-        #child.sendline("wlan {}".format(ssid))
-        #child.expect(".*config-wlan.*")
-        #if state == 0:
-        #    child.sendline("hide-ssid")
-        #    child.expect(".*config-wlan.*")
-        #else:
-        #    child.sendline("no hide-ssid")
-        #    child.expect(".*config-wlan.*")
-        #child.sendline("end\n")
-        remote_conn = remote_conn_pre.invoke_shell()
-        remote_conn.send("\n")
-        time.sleep(1)
-        remote_conn.send("{}\n".format(username))
-        remote_conn.send("{}\n".format(password))
-        time.sleep(1)
-        remote_conn.send("enable force\n")
-        remote_conn.send("conf\n")
-        time.sleep(1)
-        remote_conn.send("wlan {}\n".format(ssid))
-        time.sleep(1)
-        if state == 0:
-            remote_conn.send("hide-ssid\n")
-            time.sleep(1)
-        else:
-            remote_conn.send("no hide-ssid\n")
-            time.sleep(1)
-        remote_conn.send("end\n")
-        time.sleep(1)
-        remote_conn.close()
-    elif vendor=="openwrt":
-        remote_conn = remote_conn_pre.invoke_shell()
-        remote_conn.send("sudo -s\n")
-        time.sleep(1)
-        remote_conn.send('{}\n'.format(password))
-        time.sleep(1)
-        if state == 0:
-            remote_conn.send('uci set wireless.@wifi-device[0].disabled=1; uci commit wireless; wifi\n')
-            time.sleep(1)
-        else:
-            remote_conn.send('uci set wireless.@wifi-device[0].disabled=0; uci commit wireless; wifi\n')
-            time.sleep(1)
-        remote_conn.close()
-    elif vendor=="meraki":
-        murl='https://n150.meraki.com/api/v0/organizations/616518/networks/N_647392446434529213/ssids/0'
-        headers = {
-            'X-Cisco-Meraki-API-Key': 'b21b5c3bfa37f5d920831f11775a321c077e71d1',
-            'Content-Type': 'application/json'
-        }
-        if state==0:
-            putdata = {'enabled': False}
-        else:
-            putdata = {'enabled': True}
-        dashboard = requests.put(murl, data=json.dumps(putdata), headers=headers)
-        print(dashboard.text)
-    #elif vendor=="bred":
-
-
-def sshp_shell(device_ip):
-    username = 'mgmt'
-    password = 'Ve7petrU'
-    remote_conn_pre = paramiko.SSHClient()
-    remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    remote_conn_pre.connect(device_ip, username=username, password=password, look_for_keys=False, allow_agent=False)
-    remote_conn = remote_conn_pre.invoke_shell()
-    remote_conn.keep_this = remote_conn_pre
-    return remote_conn
-
-def sshp_rcmd(device_ip):
-    username = 'mgmt'
-    password = 'Ve7petrU'
-    remote_conn_pre = paramiko.SSHClient()
-    remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    remote_conn_pre.connect(device_ip, username=username, password=password, look_for_keys=False, allow_agent=False)
-    remote_conn_pre.keep_this = remote_conn_pre
-    return remote_conn_pre
-'''
-
 
 def detail(request,name):
     a = ssid.objects.get(name=name)

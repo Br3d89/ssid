61bb8edc129d731449da85de5af08b4c1747aae6
Konstantin <br3d89@ukr.net>
2017-02-15 12:02:36 +0200

    added jquery features

--- ssidswitcher1/ssid/views.py
+++ ssidswitcher1/ssid/views.py
@@ -97,7 +97,7 @@ def cisco(up_new, down_new, ssid_objects, i, ssid_status, errors, t=0):
         child.sendline('y')
         print('Backend success')
     except pexpect.exceptions.TIMEOUT as err:
-        errors.append(ssid_objects)
+        #errors.append(ssid_objects)
         errors.append(list(ssid_objects.values_list('name', flat=True)))
         #continue
         # print('Error Br3d',err)

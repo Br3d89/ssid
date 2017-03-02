ecb4f2a7f0921accff7497a8aabbc808e7ce151e
Konstantin <br3d89@ukr.net>
2017-03-02 11:20:24 +0200

    changed index.html

--- ssidswitcher1/ssid/views.py
+++ ssidswitcher1/ssid/views.py
@@ -398,9 +398,22 @@ def ssid_error(request):
     return JsonResponse({'ssid_error_list': ssid_error_list})
 
 
-def login():
-    pass
+def login(request):
+    args={}
+    if request.POST:
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            args['login_error']='Пользователь не найден'
+            return render(request,'login.html', args)
+    else:
+        return render(request, 'login.html', args)
 
 
-def logout():
-    pass
+def logout(request):
+    auth.logout(request)
+    return redirect('/')

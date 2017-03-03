edb23c3decc53525d67914cd9db61b2e371983d1
br3d89 <br3d89@gmail.com>
2017-03-03 13:58:49 +0200

    Remove duplicated directory

--- django_start/zen/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ZenConfig(AppConfig):
-    name = 'zen'
--- urlshare/imgshare/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import Img
-
-admin.site.register(Img)

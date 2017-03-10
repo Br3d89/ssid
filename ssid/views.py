6ebab51610be5b3c0338be2e64753157f002cc75
br3d <br3d89@gmail.com>
2017-03-10 17:34:23 +0200

    Added color to server button with active ssids

--- ssid/views.py
+++ ssid/views.py
@@ -451,12 +451,12 @@ def index(request,args={}):
     all_list = list(ssid.objects.values_list('name', flat=True))
     all_up_ssids = list(ssid.objects.values_list('name', flat=True).filter(status='1'))
     servers_with_up_ssids=list(ssid.objects.values_list('web', flat=True).distinct().filter(status='1'))
-    servers_with_down_ssids = list(ssid.objects.values_list('web', flat=True).distinct().filter(status='0'))
-    servers_ssids_sorted=list(ssid.objects.values_list('web', flat=True).distinct().filter(status='1'))
-    for i in servers_ssids_sorted:
-        if i not in servers_with_down_ssids:
-            servers_ssids_sorted.append(i)
-    print(servers_ssids_sorted)
+    #servers_with_down_ssids = list(ssid.objects.values_list('web', flat=True).distinct().filter(status='0'))
+    #servers_ssids_sorted=list(ssid.objects.values_list('web', flat=True).distinct().filter(status='1'))
+    #for i in servers_ssids_sorted:
+    #    if i not in servers_with_down_ssids:
+    #        servers_ssids_sorted.append(i)
+    #print(servers_ssids_sorted)
     errors=[]
     ctx = {}
     ctx.update(args)

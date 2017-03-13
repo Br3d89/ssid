82fdc5bf1a183f8dd16a3cab8be7457967dc6bcf
br3d <br3d89@gmail.com>
2017-03-13 16:49:39 +0200

    Added new database

--- ssid/admin.py
+++ ssid/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from .models import ssid
 
+class SsidAdmin(admin.ModelAdmin):
+    fields = ('name', 'status', 'vendor', 'ip','web')
+
+
 admin.site.register(ssid)
 

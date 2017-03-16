7cd4f6fcf0fa14c7228b915f6fe7396fe0e5b8e6
br3d <br3d89@gmail.com>
2017-03-16 11:26:24 +0200

    Added search field to admin

--- ssid/admin.py
+++ ssid/admin.py
@@ -6,6 +6,7 @@ from .models import ssid
 class SsidAdmin(admin.ModelAdmin):
     fields = ('name', 'status', 'wlan_id', 'vendor', 'ip','web')
     list_display = ('name', 'web','ip','status','vendor')
+    search_fields = ('name','web','ip','vendor')
 
 
 admin.site.register(ssid, SsidAdmin)

c4492456e09c41e3bb4afb3d8db7f10cff03df0e
br3d <br3d89@gmail.com>
2017-03-21 13:51:01 +0200

    Added groups to admin pannel

--- ssid/models.py
+++ ssid/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User,Group
 # Create your models here.
 
 class vendor(models.Model):
-    vendor = models.CharField(max_length=120, default="",verbose_name="SSID_VENDOR")
+    vendor = models.CharField(max_length=120,unique=True, default="",verbose_name="SSID_VENDOR")
 
     def __str__(self):
         return '{}'.format(self.vendor)

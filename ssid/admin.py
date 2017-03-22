a8f5c0a817ddf3a3e5d903aba6e2db730287bcab
br3d <br3d89@gmail.com>
2017-03-22 16:33:20 +0200

    Changed admin

--- ssid/admin.py
+++ ssid/admin.py
@@ -3,11 +3,17 @@ from django.contrib import admin
 from .models import ssid,vendor,device_ip,auth_server
 
 
+#class ProductAdmin(admin.ModelAdmin):
+#    list_display = ('name', 'category__level', 'category')
+
+#admin.site.register(Product, ProductAdmin)
+
+
 class SsidAdmin(admin.ModelAdmin):
-    fields = ('name', 'status', 'wlan_id', 'vendor', 'ip','web')
-    list_display = ('name', 'web.name','ip.name','status','vendor')
+    fields = ('name', 'status', 'wlan_id', 'vendor__name', 'device_ip__name','auth_server__name')
+    #list_display = ('name', 'web.name','ip.name','status','vendor')
     #search_fields = ('name', 'web', 'ip', 'vendor', 'group', 'ap_mac')
 
 
-admin.site.register(ssid, SsidAdmin)
-admin.site.register([vendor,device_ip,auth_server])
+#admin.site.register(ssid, SsidAdmin)
+admin.site.register([ssid,vendor,device_ip,auth_server],SsidAdmin)

a8682af218c2032a5b17577ea9dadfa485bea171
br3d <br3d89@gmail.com>
2017-03-22 16:22:56 +0200

    Changed admin fields

--- ssid/admin.py
+++ ssid/admin.py
@@ -6,6 +6,7 @@ from .models import ssid,vendor,device_ip,auth_server
 class SsidAdmin(admin.ModelAdmin):
     fields = ('name', 'status', 'wlan_id', 'vendor', 'ip','web')
     list_display = ('name', 'web.name','ip.name','status','vendor')
+    search_fields = ('name', 'web', 'ip', 'vendor', 'group', 'ap_mac')
 
 
 admin.site.register(ssid, SsidAdmin)

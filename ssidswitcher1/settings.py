4a10b29c6123068c4cab57c685ea9279c5705143
br3d <br3d89@gmail.com>
2017-03-24 11:16:56 +0200

    Added radio macs to all ssids

--- ssidswitcher1/settings.py
+++ ssidswitcher1/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_python3_ldap'
+    'django_python3_ldap',
     'ssid'
 ]
 

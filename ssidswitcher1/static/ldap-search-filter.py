0256eb088f4c9af7ef56cfad5cc76a23dfa7a79e
br3d <br3d89@gmail.com>
2017-04-10 18:03:34 +0300

    added ldap filter

--- ssidswitcher1/static/ldap-search-filter.py
+++ ssidswitcher1/static/ldap-search-filter.py
@@ -6,6 +6,6 @@ def custom_format_search_filters(ldap_fields):
     # Call the base format callable.
     search_filters = format_search_filters(ldap_fields)
     # Advanced: apply custom LDAP filter logic.
-    search_filters.append("(|(memberOf=noc)(memberOf=soc))")
+    #search_filters.append("(|(memberOf=noc)(memberOf=soc))")
     # All done!
     return search_filters

1ab5b1f6cc32b727135e25a3f99a61bfd0e21672
br3d <br3d89@gmail.com>
2017-04-10 18:10:33 +0300

    added ldap filter

--- ssidswitcher1/static/ldap-search-filter.py
+++ ssidswitcher1/static/ldap-search-filter.py
@@ -2,11 +2,11 @@ from django_python3_ldap.utils import format_search_filters
 
 def custom_format_search_filters(ldap_fields):
     # Add in simple filters.
-    ldap_fields["memberOf"] = "Guests"
+    ldap_fields["memberOf"] = "CN=Guests,CN=Builtin,DC=net,DC=wi-fi-bar,DC=com"
     # Call the base format callable.
     search_filters = format_search_filters(ldap_fields)
     # Advanced: apply custom LDAP filter logic.
-    search_filters.append("(|(memberOf=noc)(memberOf=soc))")
+    #search_filters.append("(|(memberOf=noc)(memberOf=soc))")
     # All done!
     print(search_filters)
     return search_filters

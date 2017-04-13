746522de0c4e59dae59843ced16e08110eb4de04
br3d <br3d89@gmail.com>
2017-04-13 18:10:47 +0300

    Added new login form

--- ssid/admin.py
+++ ssid/admin.py
@@ -13,10 +13,14 @@ class SsidAdmin(admin.ModelAdmin):
     #Shows fields that can be changed
     fields = ('name', 'wlan_id','ap_mac', 'vendor', 'ip','web','group')
     #Shows fields in admin pannel
-    list_display = ('name', 'web','ip','status','vendor')
+    list_display = ('name', 'web','ip','status','vendor','ssid_group')
     #list_filter = ('name','web')
     #search_fields = ('name', 'web', 'ip', 'vendor', 'group', 'ap_mac')
     search_fields = ('name','web__name','vendor__name','ip__name','ap_mac',)
+
+    def ssid_group(self, obj):
+        return "\n".join([p.name for p in obj.group.all()])
+
 
 class AuthServerAdmin(admin.ModelAdmin):
     list_display=('name','ip','server_group')

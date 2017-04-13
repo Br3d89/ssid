87919c05c5fd3f91f21c0d1b6a8cb670232f8c9c
br3d <br3d89@gmail.com>
2017-04-13 10:20:41 +0300

    Added auth server admin view fields

--- ssid/admin.py
+++ ssid/admin.py
@@ -10,12 +10,17 @@ from .models import ssid,vendor,device_ip,auth_server
 
 
 class SsidAdmin(admin.ModelAdmin):
+    #Shows fields that can be changed
     fields = ('name', 'wlan_id','ap_mac', 'vendor', 'ip','web','group')
+    #Shows fields in admin pannel
     list_display = ('name', 'web','ip','status','vendor')
     #list_filter = ('name','web')
     #search_fields = ('name', 'web', 'ip', 'vendor', 'group', 'ap_mac')
     search_fields = ('name','web__name','vendor__name','ip__name','ap_mac',)
 
+class AuthServerAdmin(admin.ModelAdmin):
+    list_display=('name','ip','group')
 
 admin.site.register(ssid, SsidAdmin)
-admin.site.register([vendor,device_ip,auth_server])
+admin.site.register(auth_server,AuthServerAdmin)
+admin.site.register([vendor,device_ip])

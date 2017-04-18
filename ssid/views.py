2be89ba9b0a4822aa0606a0b5a5e4e0b380e1f23
br3d <br3d89@gmail.com>
2017-04-18 12:27:24 +0300

    Changed detail.html page

--- ssid/views.py
+++ ssid/views.py
@@ -489,6 +489,7 @@ def index(request,args={}):
        for i in range(len(servers_with_up_ssids + servers_with_down_ssids)):
            div.append([])
            iter_list.append(i)
+       print('Printing servers list range', len(servers_with_up_ssids + servers_with_down_ssids))
        print('Printing div ',div)
        div_cycle = itertools.cycle(iter_list)
        for i in servers_ssids_sorted:

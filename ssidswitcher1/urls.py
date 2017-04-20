ee6fd75e8bfe2ce0a7e9b1519ac96ecb04cad655
br3d <br3d89@gmail.com>
2017-04-20 11:41:11 +0300

    Added meta to datebase

--- ssidswitcher1/urls.py
+++ ssidswitcher1/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     url(r'^ssid_busy/', ssid_busy),
     url(r'^ssid_error/', ssid_error),
     url(r'^auth/login/',login),
-    url(r'^auth/profile/$', profile),
+    url(r'^profile', profile),
     url(r'^auth/logout/', logout),
     url(r'^', index)
 ]

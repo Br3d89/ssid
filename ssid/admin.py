8c15a8747ac43a6e87ef002fb436f32df2d1b811
br3d <br3d89@gmail.com>
2017-04-28 16:10:23 +0300

    Added "create" classmethod

--- ssid/admin.py
+++ ssid/admin.py
@@ -35,7 +35,10 @@ class SsidAdmin(admin.ModelAdmin):
     search_fields = ('name','web__name','vendor__name','ip__name','ap_mac','auth_scheme__name')
 
     def get_changeform_initial_data(self, request):
-        return {'name': 'Vasya_test_123'}
+        return {'name': 'test_aaa'}
+
+    def test_aaa(self):
+        return 'olololo'
     #def ssid_group(self, obj):
     #    return "\n".join([p.name for p in obj.group.all()])
 

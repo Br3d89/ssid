8c694e583f06922992042e569240bb2da284a5a1
br3d <br3d89@gmail.com>
2017-05-17 16:46:26 +0300

    Changed function Cisco for universal implementation.

--- ssid/views.py
+++ ssid/views.py
@@ -659,7 +659,7 @@ def ssid_add(request):
         ssid_vendor = json.loads(request.POST.get('vendor'))
         ssid_device=json.loads(request.POST.get('device'))
         ssid_server = request.POST.get('server')
-        ssid_server_object=server_queryset.filter(name=ssid_server)
+        ssid_server_object=auth_server.objects.get(name=ssid_server)
         ssid_device_objects = device_queryset.filter(name__in=ssid_device)
         if ssid_device:
             ssid_vendor=list(device_queryset.filter(name__in=ssid_device).values_list('vendor__name', flat=True))

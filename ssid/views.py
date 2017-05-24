c30fcf7ceef09c9d02c091632beebfee6d9593e2
br3d <br3d89@gmail.com>
2017-05-24 14:29:13 +0300

    New APP logic **********************

--- ssid/views.py
+++ ssid/views.py
@@ -268,8 +268,8 @@ def cisco(i,ssid_objects=[], ssid_status_list=[],ssid_error_list=[], errors=[],s
             for i in ssid_objects:
                 print('Deleting wlan')
                 child.sendline('config wlan delete {}'.format(i.wlan_id))
-                child.expect('(y/n)')
                 child.sendline('y')
+                child.expect('>')
                 print('Deleting radius auth')
                 child.sendline('config radius auth delete {}'.format(wlan_radius_id_mapping[i.wlan_id]))
                 child.expect('>')

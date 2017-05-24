1b4ff9121794399ba5f010427f0550b050096d57
br3d <br3d89@gmail.com>
2017-05-24 13:56:13 +0300

    New APP logic **********************

--- ssid/views.py
+++ ssid/views.py
@@ -56,13 +56,13 @@ def ssid_update(request):
             ssid_objects_down=ssid.objects.filter(ip__name=i,name__in=down_new)
             print(ssid_objects_down)
             if ssid_objects_up:
-                p = (threading.Thread(target=globals()['{}'.format(vendor)],kwargs={'i': i,'ssid_objects': ssid_objects_up, 'ssid_status_list': ssid_status_list,'ssid_error_list': ssid_error_list, 'errors': errors,'timeout_value': timeout_value, 'action': 'enable'}))  # поменял i
+                p = (threading.Thread(target=globals()['{}'.format(vendor)],kwargs={'i': i,'ssid_objects': ssid_objects_up, 'ssid_status_list': ssid_status_list,'ssid_error_list': ssid_error_list, 'errors': errors,'ssid_timeout': timeout_value, 'action': 'enable'}))  # поменял i
                 p.start()
                 process_list.append(p)
-                d = threading.Timer(timeout_value, globals()['{}'.format(vendor)],kwargs={'i': i, 'ssid_objects': ssid_objects_up, 'ssid_status_list': ssid_status_list,'ssid_error_list': ssid_error_list, 'errors': errors,'timeout_value': timeout_value, 'action': 'disable'})  # нужно поменять i
+                d = threading.Timer(timeout_value, globals()['{}'.format(vendor)],kwargs={'i': i, 'ssid_objects': ssid_objects_up, 'ssid_status_list': ssid_status_list,'ssid_error_list': ssid_error_list, 'errors': errors,'ssid_timeout': timeout_value, 'action': 'disable'})  # нужно поменять i
                 d.start()
             if ssid_objects_down:
-                p = (threading.Thread(target=globals()['{}'.format(vendor)],kwargs={'i': i, 'ssid_objects': ssid_objects_down, 'ssid_status_list': ssid_status_list, 'ssid_error_list': ssid_error_list,'errors': errors, 'timeout_value': timeout_value,'action': 'disable'}))  # поменял i
+                p = (threading.Thread(target=globals()['{}'.format(vendor)],kwargs={'i': i, 'ssid_objects': ssid_objects_down, 'ssid_status_list': ssid_status_list, 'ssid_error_list': ssid_error_list,'errors': errors, 'ssid_timeout': timeout_value,'action': 'disable'}))  # поменял i
                 p.start()
                 process_list.append(p)
             else:
